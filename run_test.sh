96f2b422fd89980d9910ea1c8abfcab8a6e02a02
Christoph Jandl <jandl@picky.vote>
2024-10-04 11:38:17 +0200

    add more datasets

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,7 @@
 python test.py --iterations 15 --dataset_path dbp15k/fr_en/converted
 python test.py --iterations 15 --dataset_path dbp15k/ja_en/converted
 python test.py --iterations 15 --dataset_path dbp15k/zh_en/converted
+python test.py --iterations 15 --dataset_path D_W_15K_V2
+python test.py --iterations 15 --dataset_path D_Y_15K_V2
+python test.py --iterations 15 --dataset_path EN_DE_15K_V2
+python test.py --iterations 15 --dataset_path EN_FR_15K_V2
